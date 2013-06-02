8d270feb73f96036ec9b37e9aa324d150be7ac7e
Robin Sharma <sharms115@gmail.com>
2013-06-02 02:11:57 -0400

    Code cleanup, added Kik button

--- web/main_home.js
+++ web/main_home.js
@@ -1,12 +1,13 @@
 App.populator('home', function (page) {
 
   // set current page to variable x
-  var p = $(page);
+  var x = $(page);
 
   // find the location on the page where the quote will go
 
+
   //Cause app to reload one title bar pressed
-  p.find('.app-title').on('click', function() {
+  x.find('.app-title').on('click', function() {
     App.load('home', 'fade');
   });
 
@@ -17,48 +18,24 @@ App.populator('home', function (page) {
       }
     });
   });
-  function loadquote(data){
 
-    /* Unreal SlideViewer
-    - some maths to make slideViewer to function incoherent with topBar & titleBar;
-    */
-    var wrapper = page.querySelector('.wrapper');
-
-    var height = (p.height() - (p.find(".titleBar").height() + p.find(".app-topbar").height()));
-    wrapper.innerHTML = '';
-    wrapper.style.height = height + "px";
-
-/*
-    var slideViewer = new SlideViewer(wrapper, source,{startAt: 0, length: 30});
-    p.find(".app-button.right").click(function(){
-      k = slideViewer.page();
-
+  function loadquote(data) {
+    x.find('.app-section').html(data[0].description);
+    
+    x.find('.app-button.kik.right').click(function(){
       cards.kik.send({
-          title: data[k].title,
-          text: 'So funny it\'s UNREAL',
-          linkData: JSON.stringify(data[k])
+        title     : 'Message title'        ,
+        text      : 'Message body'         ,
+        big       : true                   , // optional
+        noForward : true                   , // optional
+        //linkData  : JSON.stringify(data[p])
       });
     });
-
-
-    slideViewer.on('flip', function(i){
-         if (i >= 0){
-              p.find('.titleBar').html(data[i].title);
-         }else {
-              return;
-         }                 
-    });
-*/
-
-    /*
-    - Force dat SlideViewer to set the title of the first post
-    */
-    p.find('.titleBar').html(data[0].description);
   }
 
 });
 
-App.populator('page2', function (page) {
+App.populator('About', function (page) {
   // put stuff here
 });
 
