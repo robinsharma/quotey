a097f6cf5978a80be5619d4a5dadb4461b4325c1
Robin Sharma <sharms115@gmail.com>
2013-06-02 18:03:40 -0400

    kik button test

--- web/main_home.js
+++ web/main_home.js
@@ -1,11 +1,13 @@
 App.populator('home', function (page) {
 
   var x = $(page); // set current page to variable x
+  var kik_button = $(x).find('.app-button.kik.right');
   var quote = "\"And on the eleventh day <b>quotey</b> was loading...\" - luckysharms"; // find the location on the page where the quote will go
   var quote_div = $(x).find('.quote-text');
+  
   quote_div.html(quote); // Default, placeholder quote.
   //quote_div.html("You only live once, but if you do it right, once is enough. - Mae West");
-
+  
   //Cause app to reload one title bar pressed
   $(x).find('.app-title').on('click', function() {
     App.load('home', 'fade');
@@ -26,7 +28,7 @@ App.populator('home', function (page) {
     quote_div.html(quote);
   } 
 
-  $(x).find(".app-button.kik.right").click( function () {
+  kik_button.click( function () {
     quote_div.html("<b>test</b>");
     cards.kik.send({
       title : 'Incoming Message!' ,
