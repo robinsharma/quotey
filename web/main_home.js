28806dd8c1de9b708ae99c0307c685bcca30031f
Robin Sharma <sharms115@gmail.com>
2013-06-02 03:05:23 -0400

    Added styling, general clean up

--- web/main_home.js
+++ web/main_home.js
@@ -4,7 +4,11 @@ App.populator('home', function (page) {
   var x = $(page);
 
   // find the location on the page where the quote will go
+  var quote_div = x.find('.fortune-text');
 
+  // Default, placeholder quote.
+  quote_div.html("\"And on the eleventh day <b>quotey</b> was loading...\" - luckysharms")
+  //quote_div.html("You only live once, but if you do it right, once is enough. - Mae West");
 
   //Cause app to reload one title bar pressed
   x.find('.app-title').on('click', function() {
@@ -20,7 +24,7 @@ App.populator('home', function (page) {
   });
 
   function loadquote(data) {
-    x.find('.app-section').html(data[0].description);
+    quote_div.html(data[0].description);
   }
 
 });
