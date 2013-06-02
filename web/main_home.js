f1f29863790e9a2a3ef533a0133f707254f3b6b2
Robin Sharma <sharms115@gmail.com>
2013-06-02 04:04:42 -0400

    Fixed up stuff

--- web/main_home.js
+++ web/main_home.js
@@ -7,7 +7,7 @@ App.populator('home', function (page) {
   var quote_div = x.find('.quote-text');
 
   // Default, placeholder quote.
-  quote_div.text("\"And on the eleventh day <b>quotey</b> was loading...\" - luckysharms")
+  quote_div.html("\"And on the eleventh day <b>quotey</b> was loading...\" - luckysharms");
   //quote_div.html("You only live once, but if you do it right, once is enough. - Mae West");
 
   //Cause app to reload one title bar pressed
@@ -19,13 +19,13 @@ App.populator('home', function (page) {
   cards.ready(function (){
     zAPI.getData(function(meta, quote){
       if(quote){
-        loadquote(quote)
+        loadquote(quote);
       }
     });
   });
 
   function loadquote(data) {
-    quote_div.text(data[0].description);
+    quote_div.html(data[0].description);
   }
 
 });
